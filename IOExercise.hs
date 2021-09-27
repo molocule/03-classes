909d449c195c0098b1dadd96a399e228f0c7b0eb
Claudia Zhu <jiyunzhu@seas.upenn.edu>
2021-09-27 13:07:57 -0400

    in class exercise

--- IOExercise.hs
+++ IOExercise.hs
@@ -15,36 +15,29 @@ Rewrite these programs so that they do not use 'do'.
 (Make sure that you do not change their behavior!)
 -}
 
-
-
--- simpleProgram :: IO ()
--- simpleProgram = do
---   putStrLn "This is a simple program that does IO."
---   putStrLn "What is your name?"
---   inpStr <- getLine
---   putStrLn $ "Welcome to Haskell, " ++ inpStr ++ "!"
-
 simpleProgram :: IO ()
-simpleProgram >>= 
-  putStrLn "This is a simple program that does IO." >>
-  putStrLn "What is your name?" >> 
-  getLine >>= (
-    \inpStr -> putStrLn $ "Welcome to Haskell, " ++ inpStr ++ "!"
-  )
+simpleProgram =
+  putStrLn "This is a simple program that does IO."
+    >> putStrLn "What is your name?"
+    >> getLine
+      >>= ( \inpStr -> putStrLn $ "Welcome to Haskell, " ++ inpStr ++ "!"
+          )
 
 lengthProgram :: IO Int
-lengthProgram = 
-  x >>= (length [1, 2, 3, 4, 5, 6] >> 
-  putStrLn $ "The length of the list is" ++ show x >>
-  return x)
+lengthProgram =
+  let x = length [1, 2, 3, 4, 5, 6]
+   in putStrLn ("The length of the list is" ++ show x)
+        >> return x
 
 anotherProgram :: IO ()
-anotherProgram = do
+anotherProgram =
   putStrLn "What is your name?"
-  inpStr <- getLine
-  if inpStr == "Haskell"
-    then do
-      putStrLn "You rock!"
-      return ()
-    else putStrLn $ "Hello " ++ inpStr
-  putStrLn "That's all!"
+    >> getLine
+      >>= ( \inpStr ->
+              if inpStr == "Haskell"
+                then
+                  putStrLn "You rock!"
+                    >> return ()
+                else putStrLn $ "Hello " ++ inpStr
+          )
+    >> putStrLn "That's all!"
